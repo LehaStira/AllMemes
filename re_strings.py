6aa425ee7b5505ef983d2dde41452e60189b4219
leha_stira <leha_stira@ukr.net>
2021-03-01 23:04:39 +0200

    Adding delete_last_dash for slug in models

--- re_strings.py
+++ re_strings.py
@@ -6,6 +6,8 @@ class ReformatStrings:
         :param s: String s
         :return: String s without repeating dashes
         """
+        if not isinstance(s, str): # In future - do it by decorator
+            raise ValueError(f'Error! Parametr s - {s} is not string')
         new_string = ''
         for ind, i in enumerate(s):
             if i == '-' and s[ind - 1] == '-':
@@ -14,4 +16,15 @@ class ReformatStrings:
                 new_string += 1
         return new_string
 
-
+    def delete_last_dash(self, s):
+        """
+        Deleting last symbol into s, if it - dash
+        :param s: String, that maybe has dash in the end
+        :return: String, that have not dash in the end
+        """
+        if not isinstance(s, str):  # In future - do it by decorator
+            raise ValueError(f'Error! Parametr s - {s} is not string')
+        if s[-1] == '-':
+            return s[:-1]
+        else:
+            return s
